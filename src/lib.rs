064b224063d222b268376ba65886956e8ac6ded6
Hudson Hadley <hudsonhadley@yahoo.com>
2024-08-11 15:43:23 -0400

    Properly parses the command line arguments using clap

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use clap::{ Arg, Command };
+
+pub fn config() -> (String, usize) {
+    let m = Command::new("foo")
+        .arg(Arg::new("url")
+            .short('u')
+            .long("url")
+            .required(true))
+        .arg(Arg::new("number")
+            .short('n')
+            .default_value("10"))
+        .get_matches();
+
+    let path: &String = m.get_one("url").expect("Did not specify path");
+    let number: &String = m.get_one("number").unwrap();
+    let number: usize = number.parse().unwrap();
+
+    (String::from(path), number)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
-use std::env::args;
 use reqwest;
 use tokio;
 
+use httploadtesting;
+
+
+
 #[tokio::main]
 async fn main() {
-    let mut args = args();
-    args.next();
-
-    let path = match args.len() {
-        0 => panic!("No path given"),
-
-        1 => args.next().unwrap(),
-
-        _ => panic!("More than 1 argument given, only one necessary"),
-    };
-
+    let (path, number) = httploadtesting::config();
 
     let status = reqwest::get(path)
         .await
@@ -23,6 +16,4 @@ async fn main() {
         .as_u16();
 
     println!("Response code: {status}");
-
-
 }
