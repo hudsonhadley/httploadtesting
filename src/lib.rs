ba14c4043773907e7dbc3798f4b795fa88befff4
Hudson Hadley <hudsonhadley@yahoo.com>
2024-08-12 12:27:53 -0400

    User can specify a file of urls to test sa a bulk. Data will be printed out for each url

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use clap::{Arg, Command };
 use reqwest;
 use reqwest::Error;
+use std::fs;
 
 #[derive(Clone)]
 pub struct Config {
-    url: String,
+    urls: Vec<String>,
     number: usize,
     threads: usize,
 }
@@ -12,20 +13,49 @@ pub struct Config {
 impl Config {
     pub fn new() -> Config {
         let m = Command::new("foo")
+
             .arg(Arg::new("url")
                 .short('u')
-                .long("url")
-                .required(true))
+                .long("url"))
+
+            .arg(Arg::new("file")
+                .short('f')
+                .long("file"))
+
             .arg(Arg::new("number")
                 .short('n')
                 .default_value("10"))
+
             .arg(Arg::new("threads")
                 .short('c')
                 .long("concurrent_threads")
                 .default_value("1"))
+
             .get_matches();
 
-        let path: &String = m.get_one("url").expect("Did not specify path");
+        let url: Option<&String> = m.get_one("url");
+        let file: Option<&String> = m.get_one("file");
+
+        let urls: Vec<String> = match (url, file) {
+            (Some(_), Some(_)) => panic!("File and url provided, only one required"),
+            (None, None) => panic!("No url provided"),
+
+            // Url is provided, but no file
+            (Some(url), None) => vec![url.clone()],
+
+            // File is provided
+            (None, Some(file)) => {
+                // Urls should be on a new line for each one
+                let file_string = fs::read_to_string(file).expect("No file found");
+
+                let v: Vec<String> = file_string
+                    .lines()
+                    .map(|s| String::from(s)) // Convert from &str to String
+                    .collect();
+
+                v
+            }
+        };
 
         let number: &String = m.get_one("number").unwrap();
         let number: usize = number.parse().unwrap();
@@ -35,7 +65,7 @@ impl Config {
 
 
         Config {
-            url: String::from(path),
+            urls,
             number,
             threads,
         }
@@ -49,8 +79,20 @@ impl Config {
         self.threads
     }
 
-    pub fn status(&self) -> Result<u16, Error> {
-        let response = reqwest::blocking::get(String::from(&self.url));
+    pub fn urls_len(&self) -> usize {
+        self.urls.len()
+    }
+
+    pub fn url(&self, index: usize) -> String {
+        String::from(&self.urls[index])
+    }
+
+
+    pub fn status(&self, url_index: usize) -> Result<u16, Error> {
+        let response = reqwest::blocking::get(
+            String::from( &self.urls[url_index] )
+        );
+
         match response {
             Ok(r) => Ok(r.status().as_u16()),
             Err(e) => Err(e)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::sync::mpsc;
 use std::time::{SystemTime, UNIX_EPOCH};
 use threadpool::ThreadPool;
@@ -7,64 +8,11 @@ fn get_time_millis() -> u128 {
     SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()
 }
 
-fn main() {
-    let config = Config::new();
-    let pool = ThreadPool::new(config.threads());
-
-    let (tx_status, rx_status) = mpsc::channel();
-    let (tx_time, rx_time) = mpsc::channel();
-
-    for _ in 0..config.number() {
-
-        let config = config.clone();
-
-        let tx_status = tx_status.clone();
-        let tx_time = tx_time.clone();
-
-
-        pool.execute(move || {
-            let start_time = get_time_millis();
-
-            // Get the status
-            match config.status() {
-                // Could connect but may be a page failure such as 404
-                Ok(val) => {
-
-                    // See what code it is
-                    match val {
-                        200..300 => tx_status.send(true).unwrap(),
-
-                        _ => tx_status.send(false).unwrap(),
-                    };
-                },
-
-                // Error in connecting
-                _ => tx_status.send(false).unwrap(),
-            }
-
-            // Send the time it took
-            tx_time.send(get_time_millis() - start_time).unwrap();
-        });
-    }
-
-    // Receive all the connections and times
-    let mut statuses: Vec<bool> = Vec::with_capacity(config.number());
-    let mut times: Vec<u128> = Vec::with_capacity(config.number());
-
-    for _ in 0..config.number() {
-        statuses.push(
-            rx_status.recv().unwrap()
-        );
-
-        times.push(
-            rx_time.recv().unwrap()
-        );
-    }
-
+fn print_results(url: &String, statuses: &Vec<bool>, times: &Vec<u128>) {
     // Get all the successes
     let mut success_count = 0;
     for status in statuses {
-        if status {
+        if *status {
             success_count += 1;
         }
     }
@@ -79,14 +27,107 @@ fn main() {
     let max_time = *max_time as f64 / 1000.0;
 
 
-    println!("Results:");
+    println!("---------------------------------------------------------");
+    println!(" ");
+    println!("Results for `{url}`:");
     println!("  Successful Requests (2XX)...................: {}", success_count);
-    println!("  Failed Requests (4XX/5XX)...................: {}", config.number() - success_count);
-    println!("  Requests/second.............................: {:.2}",
-             config.number() as f64/ total_time);
+    println!("  Failed Requests (4XX/5XX)...................: {}", statuses.len() - success_count);
+    println!("  Requests/Second.............................: {:.2}",
+             statuses.len() as f64/ total_time);
     println!(" ");
 
     println!("Total Request Time (s) (Min, Max, Mean).......: {:.2}, {:.2}, {:.2}",
-             min_time, max_time, total_time / config.number() as f64);
+             min_time, max_time, total_time / statuses.len() as f64);
 
+    println!(" ");
+}
+
+fn main() {
+    let config = Config::new();
+    let pool = ThreadPool::new(config.threads());
+
+    let (tx_status, rx_status) = mpsc::channel();
+    let (tx_time, rx_time) = mpsc::channel();
+
+    for i in 0..config.number() {
+
+        let config = config.clone();
+
+        let tx_status = tx_status.clone();
+        let tx_time = tx_time.clone();
+
+
+        pool.execute(move || {
+            let start_time = get_time_millis();
+
+            // Get the status
+            match config.status(i % config.urls_len() /* HOW DO WE INCREMENT THIS VALUE */){
+                // Could connect but may be a page failure such as 404
+                Ok(val) => {
+
+                    // See what code it is
+                    match val {
+                        200..300 => tx_status.send(
+                            (config.url(i % config.urls_len()), true)
+                        ).unwrap(),
+
+                        _ => tx_status.send(
+                            (config.url(i % config.urls_len()), false)
+                        ).unwrap(),
+                    };
+                },
+
+                // Error in connecting
+                _ => tx_status.send(
+                    (config.url(i % config.urls_len()), false)
+                ).unwrap(),
+            }
+
+            // Send the time it took as well as what url it was
+            tx_time.send(
+                (config.url(i % config.urls_len()), get_time_millis() - start_time)
+            ).unwrap();
+        });
+    }
+
+    // Receive all the connections and times
+    let mut statuses: HashMap< String, Vec<bool> > = HashMap::with_capacity(config.number());
+    let mut times: HashMap< String, Vec<u128> > = HashMap::with_capacity(config.number());
+
+    for _ in 0..config.number() {
+        let (url, status) = rx_status.recv().unwrap();
+
+
+        if statuses.contains_key(&url) {
+            let mut v = statuses.get(&url).unwrap().clone();
+            v.push(status);
+
+            statuses.insert(url, v);
+
+            // If we haven't seen the url before, make a new list with the status
+        } else {
+            statuses.insert(url, vec![status]);
+        }
+
+
+
+        let (url, time) = rx_time.recv().unwrap();
+
+        // If we've seen the url before, add the time to the vector
+        if times.contains_key(&url) {
+            let mut v = times.get(&url).unwrap().clone();
+            v.push(time);
+
+            times.insert(url, v);
+
+            // If we haven't seen the url before, make a new list with the time
+        } else {
+            times.insert(url, vec![time]);
+        }
+    }
+
+
+    for url in statuses.keys() {
+        print_results(url, statuses.get(url).unwrap(), times.get(url).unwrap());
+    }
 }
